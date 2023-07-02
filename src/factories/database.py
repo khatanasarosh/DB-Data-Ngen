46162c6a94b201e361ad7adb783874d96868c8f8
khatanasarosh <saroshkhatana@gmail.com>
2023-07-02 12:56:55 +0100

    renames DBFactory.create to DBFactory.get_database

--- src/factories/database.py
+++ src/factories/database.py
@@ -4,10 +4,8 @@ from src.db.mariadb import MariaDB
 from src.exceptions.invalid_db_type import InvalidDBType
 
 class DatabaseFactory:
-    def __init__(self):
-        pass
 
-    def create(self, db_type):
+    def get_database(self, db_type):
         if db_type == 'postgresql':
             return Postgres()
         elif db_type == 'mysql':
