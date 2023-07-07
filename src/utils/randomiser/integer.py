fbe2b8ad608303b5b4b5a6c74edbb60621329854
khatanasarosh <saroshkhatana@gmail.com>
2023-07-07 01:09:35 +0100

    integer randomised to number of digits of input

--- src/utils/randomiser/integer.py
+++ src/utils/randomiser/integer.py
@@ -3,8 +3,8 @@ from src.utils.randomiser.randomiser import Randomiser
 import random
 
 class IntegerRandomiser(Randomiser):
-    def randomise(self, input):
-        if isinstance(input, int):
-            return random.randint(0, input)
-        else:
-            return type(input)
+    def randomise(self, value: int) -> int:
+        lower_limit = int('1' + ('0' * (len(str(value)) - 1)))
+        upper_limit = int('9' * len(str(value)))
+
+        return random.randint(lower_limit, upper_limit)
